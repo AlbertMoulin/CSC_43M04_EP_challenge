84d43f9cd5bca65aad3a48e181eec1f72d0d9b36
AlbertMoulin <albert.moulin@polytechnique.edu>
2025-05-22 09:03:07 +0200

    deteted test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-data = pd.read_csv("submission.csv")
-# count number of negative values in the views column
-print((data["views"] < 0).sum())
-# print the index of the first 10 negative values
-print(data[data["views"] < 0].index[:10])
